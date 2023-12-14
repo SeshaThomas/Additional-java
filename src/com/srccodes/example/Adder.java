5461079f6a1c05f15a2f10e8b4416ce115a7f1fb
DEll <DEll@SSTDELL2021>
2023-12-14 15:10:33 +0530

    commiting second

---
+++ src/com/srccodes/example/Adder.java
@@ -0,0 +1,25 @@
+package com.srccodes.example;
+
+public class Adder {
+	public static int add(int x,int y) {
+		
+		int add=x+y;
+		return add;
+		
+	} 
+
+		//static int x = 10;
+		//static int y=20;
+		//static int sum;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ //sum=x+y;
+ //System.out.println(sum);
+		int a=add(10,20);
+		System.out.println(a);
+		
+		
+	}
+ 
+}
